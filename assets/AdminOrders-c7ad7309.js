fbb7c977ce13b5248120c73bec3804baefe488f8
rinnyanya <alicerinnya@gmail.com>
2023-02-25 20:49:35 +0800

    deploy week6

---
+++ assets/AdminOrders-c7ad7309.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-b1b75b9c.js";const n={};function o(t,s){return c(),r("h2",null,"後臺訂單列表")}const a=e(n,[["render",o]]);export{a as default};
---
+++ assets/AdminProducts-bbd36a0c.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as n}from"./index-b1b75b9c.js";const o={};function r(t,s){return e(),n("h2",null,"後臺產品列表")}const a=c(o,[["render",r]]);export{a as default};
